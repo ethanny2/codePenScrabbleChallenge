8128492e5cad26e07826d8ebec21f32adcfbaee7
Ethan <ethanny2@gmail.com>
2019-11-28 17:43:42 -0500

    Cleaned up version for codepen

--- js/scrabbleLite.js
+++ js/scrabbleLite.js
@@ -1,10 +1,3 @@
-
-const apiBaseUrl = 'https://od-api.oxforddictionaries.com/api/v2';
-/* Free for up to 1000 requests a month and then it should be throttled; to continue playing insert your
-own id and key which you can get from https://developer.oxforddictionaries.com/ for free */
-
-const YOUR_API_ID = '';
-const YOUR_API_KEY = '';
 /*
  Distributon of tiles found here
  https://www.thesprucecrafts.com/how-many-letter-tiles-are-in-scrabble-410933
@@ -31,27 +24,25 @@ function getLetterValue(letter) {
   }
 }
 
-function displayScoreEffect(points){
-    console.log('Displaying score effect');
-    let anim = document.getElementById('score-animation');
-    anim.innerText = '';
-    if(!anim.classList.contains('scoredClass')){
-        anim.classList.add('scoredClass');
-    }
-    //Clone to replay animation
-    let newNode = anim.cloneNode(true);
-    newNode.innerText = `Nice +${points}!`;
-    anim.remove();
-    document.body.append(newNode);
+function displayScoreEffect(points) {
+  console.log('Displaying score effect');
+  let anim = document.getElementById('score-animation');
+  anim.innerText = '';
+  if (!anim.classList.contains('scoredClass')) {
+    anim.classList.add('scoredClass');
+  }
+  //Clone to replay animation
+  let newNode = anim.cloneNode(true);
+  newNode.innerText = `Nice +${points}!`;
+  anim.remove();
+  document.body.append(newNode);
 }
 
-
-function addToScore(wordValue){
-    let score = document.getElementById('score');
-    let newScore = parseInt(score.innerText,10) + wordValue;
-    console.log(newScore);
-    score.innerText = newScore;
-    generateTiles();
+function addToScore(wordValue) {
+  let score = document.getElementById('score');
+  let newScore = parseInt(score.innerText, 10) + wordValue;
+  score.innerText = newScore;
+  generateTiles();
 }
 
 function getWordValue() {
@@ -60,41 +51,13 @@ function getWordValue() {
   );
   let total = 0;
   let word = '';
-  console.log(answerLetterArr);
   answerLetterArr.forEach(val => {
     total += parseInt(val.firstElementChild.innerText, 10);
     word += val.innerText.charAt(0);
   });
-  console.log(total);
-  console.log(word);
-  if(YOUR_API_ID !=='' && YOUR_API_KEY !== ''){
-    //validateWord(word);
-  }else{
-    addToScore(total);
-  }
+  addToScore(total);
   displayScoreEffect(total);
   return [word, total];
-}
-
-/* Validate word; checks if entered word is real through dictionary api*/
-async function validateWord(word) {
-  word = word.toLowerCase();
-  let myHeader = new Headers();
-  myHeader.append('app_id', 'apiID');
-  myHeader.append('app_key', apiKey);
-  myHeader.append('Accept', 'application/json');
-  myHeader.append('Origin', 'https://javascript.info');
-  console.log('Calling validate word');
-  const response = await fetch(
-    `https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/${word}?fields=definitions&strictMatch=false`,
-    {
-      method: 'GET',
-      headers: myHeader,
-      mode: 'cors'
-    }
-  );
-  const parsed = await response.json();
-  console.log(parsed);
 }
 
 /*Generates random letters for word based on actual scrabble data.
@@ -124,21 +87,14 @@ function generateTiles() {
     );
   }
   playTiles = [...temp];
-  console.log(playTiles);
   playTiles.forEach(tile => {
     id++;
-    //get val through map
     letterVal = getLetterValue(tile);
     markup = `<li id="tile-${id}" class="tile" >${tile} <span class="tile-value"> ${letterVal}</span></li>`;
     startRow.innerHTML += markup;
   });
-  console.log('Done generating tiles');
 }
 
-/*  Maybe 90 is good?
-    time- time in seconds
-    returns id to clear timer
-*/
 function startTimer(time) {
   let id = setInterval(() => {
     //Update view every 1s
@@ -151,20 +107,15 @@ function startTimer(time) {
 time remaining passed in in seconds*/
 function updateTimer(time, id) {
   document.getElementById('timer-num').innerHTML = `${time} `;
-  console.log('UPDATING TIMER');
   if (time <= 0) {
-    console.log('All out of time!');
     clearInterval(id);
   }
 }
 
 function gameLoop() {
   generateTiles();
-  console.log('Calling gameLoop!');
-   startTimer(120);
+  startTimer(120);
 }
-
-
 
 /* When tile clicked play animation that adds it to the answer grid */
 function insertLetter(tile) {
@@ -177,9 +128,6 @@ function insertLetter(tile) {
   inserted.classList.toggle('tileHidden');
   document.getElementById('answer-row').appendChild(inserted);
   let endRect = findAbsPos(inserted);
-
-  console.log(inserted.getBoundingClientRect());
-  console.log(startRect);
   computeVector(startRect, endRect);
   tile.classList.toggle('tileHidden');
   animation.classList.add('setTileAnim');
@@ -188,18 +136,13 @@ function insertLetter(tile) {
   setTimeout(() => {
     inserted.classList.toggle('tileHidden');
     document.body.removeChild(animation);
-    console.log(inserted.getBoundingClientRect());
-    console.log(findAbsPos(inserted));
   }, 500);
 }
 
-/* Going back animation could reuse above....
- */
 function removeLetter(tile) {
   let orignalTile = Array.from(
     document.getElementById('tile-row').children
   ).find(elem => elem.id === tile.id);
-  console.log(orignalTile);
   let animation = tile.cloneNode(true);
   let endRect = findAbsPos(orignalTile);
   let startRect = findAbsPos(tile);
@@ -207,9 +150,6 @@ function removeLetter(tile) {
   animation.style.left = startRect[0] + 'px';
   animation.style.top = startRect[1] + 'px';
   tile.remove();
-
-  console.log(startRect);
-  console.log(endRect);
   computeVector(startRect, endRect);
   animation.classList.add('removeTileAnim');
   document.body.appendChild(animation);
@@ -230,17 +170,10 @@ function computeVector(startCoords, endCoords) {
   let yDist = -(startCoords[1] - endCoords[1]); //Always negative ; moves up to answer board
   document.documentElement.style.setProperty('--tx', `${xDist}px`);
   document.documentElement.style.setProperty('--ty', `${yDist}px`);
-  console.log(
-    getComputedStyle(document.documentElement).getPropertyValue('--tx')
-  );
-  console.log(
-    getComputedStyle(document.documentElement).getPropertyValue('--ty')
-  );
 }
 
 /*https://www.quirksmode.org/js/findpos.html */
 function findAbsPos(obj) {
-  console.log(obj);
   let curleft = (curtop = 0);
   if (obj.offsetParent) {
     do {
@@ -255,7 +188,6 @@ window.addEventListener('DOMContentLoaded', event => {
   document.querySelector('#tile-row').addEventListener('click', event => {
     if (event.target && event.target.classList.contains('tile')) {
       insertLetter(event.target);
-      console.log('Calling insert');
     }
   });
   document.getElementById('refresh').addEventListener('click', event => {
@@ -265,18 +197,15 @@ window.addEventListener('DOMContentLoaded', event => {
     getWordValue();
   });
   document.addEventListener('keypress', event => {
-      if(event.keyCode == 13){
-          console.log('Enter pressed');
-        getWordValue();
-      }
+    if (event.keyCode == 13) {
+      getWordValue();
+    }
   });
 
   document.querySelector('#answer-row').addEventListener('click', event => {
     if (event.target && event.target.classList.contains('tile')) {
       removeLetter(event.target);
-      console.log('Calling remove');
     }
   });
   gameLoop();
-
 });
